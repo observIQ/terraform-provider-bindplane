9f477eb125ce055ee766b45cc5387b7a8b12ff6e
jsirianni <joe.sirianni@observiq.com>
2023-07-22 13:26:16 -0400

    start adding destinations

---
+++ example/destination_custom.tf
@@ -0,0 +1,26 @@
+resource "bindplane_destination" "custom" {
+  rollout = true
+  name = "example-custom"
+  type = "custom"
+  parameters_json = jsonencode(
+    [
+      {
+        "name": "telemetry_types",
+        "value": [
+          "Metrics",
+          "Logs",
+          "Traces"
+        ]
+      },
+      {
+        "name": "configuration",
+        "value": <<EOT
+logging:
+  verbosity: detailed
+  sampling_initial: 5
+  sampling_thereafter: 200
+EOT
+      }
+    ]
+  )
+}
