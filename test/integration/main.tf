973c3344534b1b360d260beb66d92b455c974f53
Joe Sirianni <joe.sirianni@observiq.com>
2025-02-06 19:54:52 -0500

    add larger test

--- test/integration/main.tf
+++ test/integration/main.tf
@@ -254,3 +254,125 @@ resource "bindplane_processor_bundle" "bundle" {
     name = bindplane_processor.batch.name
   }
 }
+
+resource "bindplane_source" "splunk_tcp" {
+  rollout = false
+  name    = "Splunk"
+  type    = "splunk_tcp"
+}
+
+resource "bindplane_processor" "json_parser" {
+  rollout = false
+  name    = "Parse-JSON-Body"
+  type    = "parse_json"
+}
+
+resource "bindplane_processor" "severity_parser_v2" {
+  rollout = false
+  name = "Parse-Severity-HTTP-Status-v2"
+  type = "parse_severity_v2"
+  parameters_json = jsonencode(
+  [
+    {
+      "name": "match",
+      "value": "Body"
+    },
+    {
+      "name": "body_severity_field",
+      "value": "level"
+    },
+  ]
+  )
+}
+
+resource "bindplane_processor" "time_parser" {
+  rollout = false
+  name    = "Parse-Timestamp"
+  type    = "parse_timestamp"
+  parameters_json = jsonencode(
+    [
+      {
+        "name": "telemetry_types",
+        "value": [
+          "Logs"
+        ]
+      },
+      {
+        "name": "log_field_type",
+        "value": "Body"
+      },
+      {
+        "name": "log_source_field",
+        "value": "datetime"
+      },
+      {
+        "name": "log_time_format",
+        "value": "Manual"
+      },
+      {
+        "name": "log_manual_timestamp_layout",
+        "value": "%d/%b/%Y:%H:%M:%S %z"
+      },
+    ]
+  )
+}
+
+resource "bindplane_processor" "cleanup_promoted" {
+  rollout = false
+  name    = "k8s-trace-cleanup"
+  type    = "delete_fields_v2"
+  parameters_json = jsonencode(
+    [
+      {
+        "name": "telemetry_types",
+        "value": ["Logs"]
+      },
+      {
+        "name": "resource_attributes",
+        "value": ["level","datetime"]
+      },
+    ]
+  )
+}
+
+resource "bindplane_processor_bundle" "parser" {
+  rollout = true
+  name = "generic-parse-bundle"
+
+  processor {
+    name = bindplane_processor.json_parser.name
+  }
+
+  processor {
+    name = bindplane_processor.severity_parser_v2.name
+  }
+
+  processor {
+    name = bindplane_processor.time_parser.name
+  }
+
+  processor {
+    name = bindplane_processor.cleanup_promoted.name
+  }
+}
+
+resource "bindplane_configuration" "splunk" {
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  rollout  = true
+  name     = "splunk"
+  platform = "linux"
+
+  source {
+    name = bindplane_source.splunk_tcp.name
+    processors = [
+      bindplane_processor_bundle.parser.name,
+    ]
+  }
+
+  destination {
+    name = bindplane_destination.logging.name
+  }
+}
