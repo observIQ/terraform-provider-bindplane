ce7b51b997dc4e5b218fe2d6cb1be1b8af19bdb9
Joe Sirianni <joe.sirianni@observiq.com>
2025-02-08 12:08:42 -0500

    lint: package comment

--- internal/component/component.go
+++ internal/component/component.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package component provides functions for defining bindplane
+// component values.
 package component
 
 import (
