5c7e15b852f0a26fa771744a633cad9360775b46
schmikei <keith.schmitty@gmail.com>
2025-03-03 20:01:36 -0500

    fix formatting

--- internal/parameter/parameter.go
+++ internal/parameter/parameter.go
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package parameter provides functions for marshalling and unmarshalling
-//
-//	resource parameters.
+// Package parameter provides functions for marshalling and unmarshalling resource parameters.
 package parameter
 
 import (
