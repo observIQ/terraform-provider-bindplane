5e60a7d84779e83038c78f0e07bdb012b9672dd9
Joseph Sirianni <joe.sirianni@observiq.com>
2026-05-06 14:53:43 -0400

    Use Bindplane v1.100.0 (#213)
    * Use Bindplane v1.100.0
    
    * update test containers usage
    
    * fix parse time processor usage

--- test/local/main.tf
+++ test/local/main.tf
@@ -166,7 +166,7 @@ resource "bindplane_processor" "json-parse-body" {
 resource "bindplane_processor" "time-parse-http-datatime" {
   rollout = false
   name = "time-parse-http-datatime"
-  type = "parse_timestamp"
+  type = "parse_timestamp_v2"
   parameters_json = jsonencode(
     [
       {
